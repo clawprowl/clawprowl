5476b0af6bc295875b766e0a0b60643947893d28
clawprowl <clawprowl@gmail.com>
2026-03-05 14:52:39 +0700

    Update test-setup.ts

--- src/i18n/test-setup.ts
+++ src/i18n/test-setup.ts
@@ -6,24 +6,10 @@ import enConsole from "./locales/en/console.json";
 import enLayout from "./locales/en/layout.json";
 import enOffice from "./locales/en/office.json";
 import enPanels from "./locales/en/panels.json";
-import enChat from "./locales/en/chat.json";
-import enCommon from "./locales/en/common.json";
-import enConsole from "./locales/en/console.json";
-import enLayout from "./locales/en/layout.json";
-import enOffice from "./locales/en/office.json";
-import enPanels from "./locales/en/panels.json";
 
 if (!i18n.isInitialized) {
   i18n.use(initReactI18next).init({
     resources: {
-      en: {
-        common: enCommon,
-        layout: enLayout,
-        office: enOffice,
-        panels: enPanels,
-        chat: enChat,
-        console: enConsole,
-      },
       en: {
         common: enCommon,
         layout: enLayout,
