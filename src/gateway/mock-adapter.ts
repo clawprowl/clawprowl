61f406739d4d37e8bcc71220e3b4364662116ecf
CLAW PROWL <clawprowl@gmail.com>
2026-03-11 22:16:54 +0700

    Update mock-adapter.ts

--- src/gateway/mock-adapter.ts
+++ src/gateway/mock-adapter.ts
@@ -773,14 +773,14 @@ export class MockAdapter implements GatewayAdapter {
       mainKey: "agent:main:main",
       scope: "global",
       agents: [
-        { id: "main", name: "PROWL", default: true, identity: { name: "PROWL", emoji: "🐾" } },
+        { id: "main", name: "PROWL", default: true, identity: { name: "PROWL", emoji: "🐾", avatarUrl: "https://pbs.twimg.com/profile_images/2029487683278708736/JqpyzvWW_400x400.jpg" } },
         {
           id: "ai-researcher",
           name: "0xDeployer",
-          identity: { name: "0xDeployer", emoji: "🚀" },
+          identity: { name: "0xDeployer", emoji: "🚀", avatarUrl: "https://pbs.twimg.com/profile_images/1816688728951476224/PkVN69ln_400x400.jpg" },
         },
-        { id: "coder", name: "FINN", identity: { name: "FINN", emoji: "⚡" } },
-        { id: "ecommerce", name: "BAGS", identity: { name: "BAGS", emoji: "🏦" } },
+        { id: "coder", name: "FINN", identity: { name: "FINN", emoji: "⚡", avatarUrl: "https://pbs.twimg.com/profile_images/2003998762503745536/jDpf21Ig_400x400.jpg" } },
+        { id: "ecommerce", name: "BAGS", identity: { name: "BAGS", emoji: "🏦", avatarUrl: "https://pbs.twimg.com/profile_images/2029807842870579201/TAhshEh2_400x400.jpg" } },
       ],
     };
   }
